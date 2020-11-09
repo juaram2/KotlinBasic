9164daebcda566a35b3a12212fb6f9dd55112cbb
aram ju <aaadkfka@gmail.com>
2020-11-09 16:41:23 +0900

    Button States and Snackbar

--- andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -40,6 +40,26 @@ class SleepTrackerViewModel(
     formatNights(nights, application.resources)
   }
 
+  val startButtonVisible = Transformations.map(tonight) {
+    null == it
+  }
+
+  val stopButtonVisible = Transformations.map(tonight) {
+    null != it
+  }
+
+  val clearButtonVisible = Transformations.map(nights) {
+    it?.isNotEmpty()
+  }
+
+  private var _showSnackbarEvent = MutableLiveData<Boolean>()
+  val showSnackbarEvent: LiveData<Boolean>
+    get() = _showSnackbarEvent
+
+  fun doneShowingSnackbar() {
+    _showSnackbarEvent.value = false
+  }
+
   private val _naavigateToSleepQuality = MutableLiveData<SleepNight>()
   val navigateToSleepQuality: LiveData<SleepNight>
     get() = _naavigateToSleepQuality
@@ -103,6 +123,7 @@ class SleepTrackerViewModel(
     uiScope.launch {
       clear()
       tonight.value = null
+      _showSnackbarEvent.value = true
     }
   }
 
